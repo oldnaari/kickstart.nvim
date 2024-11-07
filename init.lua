7aa3d56faf7b3cbb1223f2d63278717d97610ecd
daniilhayrapetyan <daniil.hayrapetyan@picsart.com>
2024-11-08 02:50:50 +0400

    Fixed nord theme for iterm2

--- init.lua
+++ init.lua
@@ -841,17 +841,13 @@ require('lazy').setup({
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
     'shaunsingh/nord.nvim',
 
-    opt = {
-      transparent = true,
-    },
-
     -- 'tinted-theming/tinted-vim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'default'
+      vim.cmd.colorscheme 'nord'
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
