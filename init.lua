8b6952a0b07982d9355d4f41c3712ee85d9f2bbf
daniilhayrapetyan <daniil.hayrapetyan@picsart.com>
2024-11-08 16:38:35 +0400

    Reset background to opaque to prevent distruction when coding

--- init.lua
+++ init.lua
@@ -93,7 +93,7 @@ vim.g.maplocalleader = ' '
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
 
-vim.g.nord_disable_background = true
+vim.g.nord_disable_background = false
 -- [[ Setting options ]]
 -- See `:help vim.opt`
 -- NOTE: You can change these options as you wish!
