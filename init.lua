36fc006279173963d096a1041982a22f2e1c5b3e
Daniil Hayrapetyan <31252576+oldnaari@users.noreply.github.com>
2026-01-14 17:56:37 +0400

    change mini.surround shortcuts to match tpope/surround

--- init.lua
+++ init.lua
@@ -1083,7 +1083,18 @@ require('lazy').setup({
       -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
       -- - sd'   - [S]urround [D]elete [']quotes
       -- - sr)'  - [S]urround [R]eplace [)] [']
-      require('mini.surround').setup()
+      require('mini.surround').setup({
+        mappings = {
+          add = 'ys',
+          delete = 'ds',
+          replace = 'cs',
+          -- find = 'sf',
+          -- find_left = 'sF',
+          -- highlight = 'sh',
+          -- update_n_lines = 'sul',
+        },
+        -- Other options can be configured here if needed
+      })
 
       -- Simple and easy statusline.
       --  You could remove this setup call if you don't like it,
